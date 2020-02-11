d6369d67400a1b518d3657099e47fd333182ebdc
Ming <ming@localhost>
2020-02-11 10:20:05 -0800

    Update book.js

--- book.js
+++ book.js
@@ -3,13 +3,13 @@ var pkg = require('./package.json');
 module.exports = {
     // Documentation for GitBook is stored under "docs"
     root: './docs',
-    title: '@gitbook-ng/gitbook Documentation',
+    title: '@gitbook-ng/gitbook',
 
     // Enforce use of GitBook v3
     gitbook: '3.3.4',
 
     // Use the "official" theme
-    plugins: ['theme-official@2.1.1', '-sharing', '-fontsettings', 'sitemap'],
+    plugins: ['-sharing', '-fontsettings', 'sitemap'],
 
     variables: {
         version: pkg.version
@@ -18,7 +18,10 @@ module.exports = {
     pluginsConfig: {
         sitemap: {
             hostname: 'https://gitbook-ng.github.com',
-            pathPrefix: '/docs/'
+        },
+
+        'theme-default': {
+            showLevel: false
         }
     }
 };
